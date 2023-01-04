9ddde783ce7c46a956d9f64e5a74cce8e343219d
DavidePosillipo <davide.posillipo@icloud.com>
2023-01-04 11:21:25 +0100

    added Lasso Regression

--- scripts/preprocessing_regression_models.py
+++ scripts/preprocessing_regression_models.py
@@ -33,7 +33,7 @@ class Preprocess:
     def read_dataframe(self, request_tgt):
 
         df = pd.read_parquet(self.input_data_path)
-        print('df shape',df.shape)
+
         df[self.target] = df.lpep_dropoff_datetime - df.lpep_pickup_datetime
         df[self.target]  = df[self.target].apply(lambda td: td.total_seconds() / 60)
         df = df[(df[self.target]  >= 1) & (df[self.target]  <= 60)].reset_index(drop=True) #suggested online  
@@ -53,7 +53,7 @@ class Preprocess:
         return X,Y
 
 
-    def preprocess(self, df: pd.DataFrame, enable_categorical: bool = False,  fit_ohe: bool = False, ohe: OneHotEncoder = None):
+    def preprocess(self, df: pd.DataFrame, enable_categorical: bool = False,  fit_ohe: bool = False, drop_first_column: bool = False, ohe: OneHotEncoder = None):
 
         '''
         - enable_categorical: 
@@ -61,6 +61,7 @@ class Preprocess:
             True: they will be converted as 'category' type (suitable for boosting algs that manage cat variales (es catboost or 
             xgboost with enable_categorical=True ))
         - fit_ohe: wether to fit a ohe. OSS: This parameter has effect only if enable_categorical=False
+        - drop_first_column: remove the first column of the OHE representation (to avoid multicollinearity for unregularized linear models). OSS: This parameter has effect only if enable_categorical=False
         - ohe: 
             pretrained one hot encoder, used when fit_ohe=False (use ohe=True for preporcessing in a scoring pipeline)
             OSS: This parameter has effect only if enable_categorical=False
@@ -83,7 +84,16 @@ class Preprocess:
         else:
             # one hot encoding 
             if fit_ohe:
-                ohe = OneHotEncoder(handle_unknown='ignore', sparse = False)
+                if drop_first_column:
+                    ohe = OneHotEncoder(
+                            drop='first',
+                            handle_unknown='ignore', 
+                            sparse = False)
+                else:
+                    ohe = OneHotEncoder(
+                            handle_unknown='ignore',
+                            sparse=False)
+
                 ohe.fit(df[categorical_cols])
                 cat_ohe = ohe.transform(df[categorical_cols]) # it is an array, convert it in df with column names
                 ohe_df = pd.DataFrame(cat_ohe, columns = ohe.get_feature_names_out(input_features = categorical_cols), index = df.index)
